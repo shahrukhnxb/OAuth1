8240c52a898a31080458e48e078b91282092a595
Kosso <kosso1@gmail.com>
2014-11-02 18:14:37 +0000

    Add new post creation to PHP test.

--- php_test/wp_oauth_test_auth_flow.php
+++ php_test/wp_oauth_test_auth_flow.php
@@ -9,14 +9,7 @@ Description : Simple PHP test to obtain oauth_token and oauth_token_secret (Acce
 Author      : @kosso
 Date        : Oct 29, 2014
 
-// Useful reading.. 
-// Signing Requests using HMAC-SHA1
-// https://developer.yahoo.com/oauth/guide/oauth-signing.html
-
-// Signing Requests using PLAINTEXT
-// https://developer.yahoo.com/oauth/guide/oauth-sign-plaintext.html
-
-// More useful stuff. 
+// useful for further mods.. 
 http://oauth.googlecode.com/svn/code/php/OAuth.php
 which is used by .. 
 https://developer.yahoo.com/boss/search/boss_api_guide/codeexamples.html#oauth_php
@@ -27,6 +20,10 @@ session_start();
 
 
 if($_GET['logout']==1){
+
+    setcookie("access_token", "", 1 );
+    setcookie("access_token_secret", "", 1 );
+    setcookie("user_object", "", 1 );
 
     session_destroy();
     header('Location: '.$_SERVER['PHP_SELF']);
@@ -48,10 +45,16 @@ class OAuthWP
     }
 
 
-    function getUserProfile($oauth_access_token, $oauth_access_token_secret){
+    function oauthRequest($url, $method, $oauth_access_token, $oauth_access_token_secret, $request_params = null){
 
-        $request_method = 'GET';
+        // Useful reading.. 
+        // Signing Requests using HMAC-SHA1
+        // https://developer.yahoo.com/oauth/guide/oauth-signing.html
 
+        // Signing Requests using PLAINTEXT
+        // https://developer.yahoo.com/oauth/guide/oauth-sign-plaintext.html
+
+    
         $params = array(
             "oauth_version" => "1.0",
             "oauth_nonce" => time(),
@@ -62,6 +65,16 @@ class OAuthWP
         );
 
         // ## BUILD OAUTH SIGNATURE
+
+
+        if($request_params!=null){
+            foreach ($request_params as $k => $v){
+                    $params[$k] = $v;
+            }
+            uksort($params, 'strcmp');
+        }
+
+
         
         // Encode params keys, values, join and then sort.
         $keys = $this->_urlencode_rfc3986(array_keys($params));
@@ -70,15 +83,22 @@ class OAuthWP
         uksort($params, 'strcmp');
 
         // Convert params to string 
-        foreach ($params as $k => $v) {$pairs[] = urlencode($k).'='.urlencode($v);}
+        foreach ($params as $k => $v) {$pairs[] = $this->_urlencode_rfc3986($k).'='.$this->_urlencode_rfc3986($v);}
         $concatenatedParams = implode('&', $pairs);
         $concatenatedParams = str_replace('=', '%3D', $concatenatedParams);
         $concatenatedParams = str_replace('&', '%26', $concatenatedParams);
 
         // Form base string (first key)
-        $baseString= $request_method."&".urlencode($this->uri_user)."&".$concatenatedParams;
+        $baseString= $method."&".urlencode($url)."&".$concatenatedParams;
+
+
         // Form secret (second key)
         $secret = urlencode($this->secret)."&".$oauth_access_token_secret; // concatentate the oauth_token_secret 
+
+        echo '<h3>client is using secret : '.$secret.'</h3>';
+
+        
+
 
         // Make signature and append to params
         $params['oauth_signature'] = rawurlencode(base64_encode(hash_hmac('sha1', $baseString, $secret, TRUE)));
@@ -86,20 +106,28 @@ class OAuthWP
         // Re-sort params
         uksort($params, 'strcmp');
 
+
         // Build HTTP Authenticated Request Headers  (not used yet)
         $post_headers = $this->buildAuthorizationHeader($params);
+
+        echo '<br>BASE STRING:<br><br>'.$baseString.'<br><br>PARAMS:<br><br>';
+       // echo '<br>'.$post_headers.'<br>';
+
+        print_r($params);
 
         // convert params to string 
         foreach ($params as $k => $v) {$urlPairs[] = $k."=".$v;}
         $concatenatedUrlParams = implode('&', $urlPairs);
 
         // form url
-        $url = $this->uri_user."?".$concatenatedUrlParams;
+        $final_url = $url."?".$concatenatedUrlParams;
+
+       // echo '<br><br>FINAL URL BEING CALLED BY CLIENT: <br>'.$final_url.'<br>';
 
         // Request using cURL
-        $user_object = $this->_http($url, null, $post_headers); 
+        $json_response = $this->_http($final_url, $request_params, $post_headers); 
 
-        return $user_object;
+        return $json_response;
 
     }
 
@@ -230,13 +258,21 @@ class OAuthWP
         if(isset($post_data))
         {
             curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, urlencode($post_data));
-            //$header[] = 'Content-Type: application/x-www-form-urlencoded';
+            
+            // had to use application/json, since other methods returned a missing callback paameter error.
+            $header[] = 'Content-Type: application/json';
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+            
         }
-        // ?? hmmm ... 
+
+        // Not being used yet. 
         if(isset($post_headers))
         {
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $post_headers);
+            //echo '<hr>';
+            //print_r($post_headers);
+            //echo '<hr>';
+            //curl_setopt($ch, CURLOPT_HTTPHEADER, $post_headers);
         }
 
 
@@ -244,6 +280,8 @@ class OAuthWP
         $this->http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         $this->last_api_call = $url;
         curl_close($ch);
+
+        echo '<hr>RESPONSE:<br>'.$response.'<hr>';
 
         return $response;
     }
@@ -296,15 +334,15 @@ class OAuthWP
 
 // Edit the config to your requirements.
 $oauth_config = array(
-    'key' => 'someKeyForYourApp', 
-    'secret'=>'xxxxxxxxxxxxAsecretKeyxxxxxxxxxxxxxxxxxxx',
-    'wp_api_domain'=>'http://yourdomain.com',
-    /* TODO: These should actually be 'discovered' from the /wp-json JSON in /authentication->oauth1['request|authorize|access'] ... */
-    'uri_request'=> 'http://yourdomain.com/oauth1/request',
-    'uri_authorize'=> 'http://yourdomain.com/oauth1/authorize',
-    'uri_access'=> 'http://yourdomain.com/oauth1/access',
-    'uri_user'=> 'http://yourdomain.com/wp-json/users/me'
+    'key' => 'GgbheDq6t3Mm', 
+    'secret'=>'fPnK83pry1uo0f7YiERqlXwaCXkxQP2TMf4dk3d8bgxJ8lKb',
+    'wp_api_domain'=>'http://crosspo.st',
+    'uri_request'=> 'http://crosspo.st/oauth1/request',
+    'uri_authorize'=> 'http://crosspo.st/oauth1/authorize',
+    'uri_access'=> 'http://crosspo.st/oauth1/access',
+    'uri_user'=> 'http://crosspo.st/wp-json/users/me'
 );
+
 
 
 // Set the OAuth callback to return to this url, no matter where it's run from.
@@ -313,6 +351,8 @@ if(isset($_SERVER['HTTPS'])){ $callback_protocol = 'https'; }
 $oauth_config['oauth_callback'] = $callback_protocol.'://'.$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME'];
 
 $auth = new OAuthWP($oauth_config);
+
+echo '<pre>';
 
 // Pick up url query params after the oauth_callback after request token generation. (Also added check to make sure we're coming back from the OAuth server host)
 if(isset( $_REQUEST['oauth_token'] ) && isset( $_REQUEST['oauth_verifier'] ) && parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST) == parse_url($oauth_config['uri_request'], PHP_URL_HOST)  ){
@@ -338,18 +378,23 @@ if(isset( $_REQUEST['oauth_token'] ) && isset( $_REQUEST['oauth_verifier'] ) && 
     setcookie("access_token", $access_token, time() + (3600 * 72) );                 // expire in 72 hours...
     setcookie("access_token_secret", $access_token_secret, time() + (3600 * 72));    // expire in 72 hours...
 
-    $user_object = json_decode($auth->getUserProfile($access_token, $access_token_secret));
+    $user_object = json_decode($auth->oauthRequest($oauth_config['uri_user'],'GET', $access_token, $access_token_secret));
 
     setcookie("user_object", json_encode($user_object),  time() + (3600 * 72));    // expire in 72 hours...
 
     // Uncomment for more debug
-    // echo '<h3>You appear to be logged in as : '.$user_object->username.'</h3>';
-    // echo '<h5>User_object returned via WP-API</h5>';
+    //echo '<h3>You appear to be logged in as : '.$user_object->username.'</h3>';
+    //echo '<h5>User_object returned via WP-API</h5>';
+    //print_r($user_object);
+
 
     // Send the now logged in user back to where we started...
-    header('Location : '.$_SERVER['PHP_SELF']);
+    // header('Location : '.$_SERVER['PHP_SELF']);
 
-    //exit;
+    echo '<script> window.location = "'.$_SERVER['PHP_SELF'].'";</script>';
+
+
+    exit;
 
 }
 
@@ -358,6 +403,27 @@ if(isset($_COOKIE['access_token']) && isset($_COOKIE['access_token_secret']) && 
     // Visitor already appears to have the cookies set. 
     echo '<h3>Logged in as: '.json_decode($_COOKIE['user_object'])->username.'</h3>';
     echo '<h3><a href="?logout=1">CLICK HERE TO LOG OUT</a></h3>';
+
+
+
+    echo '<h3>OK.. Now try a post.... </h3>';   
+
+   // $post_data[];
+
+    $post_data = array(
+        'status' => 'publish',
+        'title' => 'Success! It actually worked!',
+        'content_raw' => 'This is a post from a PHP client script running on another domain and server. It uses WP-API/WP-API and WP-API/Outh1.<h4>Pretty Sweet</h4>'
+    );
+
+
+    $post_object = json_decode($auth->oauthRequest($oauth_config['wp_api_domain'].'/wp-json/posts','POST', $_COOKIE['access_token'], $_COOKIE['access_token_secret'], $post_data));
+
+    echo '<hr><h3>POST /wp-json/posts Response </h3>';
+
+    print_r($post_object);
+
+
     
 } else {
     // Not logged in. 
@@ -368,6 +434,7 @@ if(isset($_COOKIE['access_token']) && isset($_COOKIE['access_token_secret']) && 
 
 }
 
+echo '</pre>';
 
 
 ?>
